42fb24a337787468d46ac335f3615618df99a36c
Sowayi <sapwao@gmail.com>
2019-05-26 10:18:29 -0400

    Add PitchForm class to enable user to create and submit their pitches

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,9 +7,12 @@ class UpdateProfile(FlaskForm):
     class UpdateProfile to model user Input on their profile information
     to match class User in the database.
     """
+    # Creating instances of form fields
     bio = TextAreaField("Tell us about yourself", validators=[Required()])
     submit = SubmitField('Submit')
     
 class PitchForm(FlaskForm):
     postedBy = StringField('Posted by', validators=[Required()])
-    
+    pitch = TextAreaField('Pitch')
+    category = SelectField('Choose a category for your pitch', choices=[('Interview', 'Interview_pitch'),('Product','Product_pitch'),('Pickup','Pickup_lines'),('promotion','promotion_pitch')])
+    submit = SubmitField('Submit')
