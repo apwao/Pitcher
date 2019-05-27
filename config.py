fb3a0b3d7a01e25abfe00c9cce3823379985f3ce
Sowayi <sapwao@gmail.com>
2019-05-27 01:26:38 -0400

    Fix indentation error in email configurations

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ class Config:
     General configuration parent class
     """
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sowayi:Sowasse@localhost/pitcher'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sowayi:Sowasse@localhost/pitcher'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     """
