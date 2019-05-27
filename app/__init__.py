7c2ee5e5f716fe0b368503c83cf60b6716564e8d
Sowayi <sapwao@gmail.com>
2019-05-27 18:37:56 -0400

    Add simpleMDE configurations to add markdown to the pitches and comments forms

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 # creating instances of the extensions
 bootstrap = Bootstrap()
@@ -14,6 +15,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 mail = Mail()
+simple = SimpleMDE()
 
 def create_app(config_name):
     """
@@ -30,6 +32,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    simple.init_app(app)
+    mail.init_app(app)
     
     # Registering blue prints
     from .main import main as main_blueprint
