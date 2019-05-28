250105b772e89925a6674e2a6a062d85a2b874bd
Sowayi <sapwao@gmail.com>
2019-05-28 11:16:01 -0400

    ssup

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='what the hell GOT'
 export MAIL_USERNAME='pasowayi@gmail.com'
 export MAIL_PASSWORD='40A79jkL3B2i'
-export DATABASE_URI='postgresql+psycopg2://sowayi:Sowasse@localhost/pitcher'
+export DATABASE_URL='postgresql+psycopg2://sowayi:Sowasse@localhost/pitcher'
 python3.6 manage.py server
