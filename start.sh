39a9e5806216eb84565911ac240cac8f2d5b89aa
Sowayi <sapwao@gmail.com>
2019-05-29 01:18:54 -0400

    Change start configurations

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 export SECRET_KEY='whatthehellgot'
-export MAIL_USERNAME='pasowayi@gmail.com'
-export MAIL_PASSWORD='40A79jkL3B2i'
 export DATABASE_URL='postgresql+psycopg2://sowayi:Sowasse@localhost/pitcher'
 python3.6 manage.py server
